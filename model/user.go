971aadbce5e9e551b4fd6be4088fbec210e40fde
wuyongxin <wuyx@tydic.com>
2026-05-28 18:29:06 +0800

    默认开启日志IP显示

--- model/user.go
+++ model/user.go
@@ -401,6 +401,7 @@ func (user *User) Insert(inviterId int) error {
 	// 初始化用户设置，包括默认的边栏配置
 	if user.Setting == "" {
 		defaultSetting := dto.UserSetting{}
+		defaultSetting.RecordIpLog = true
 		// 这里暂时不设置SidebarModules，因为需要在用户创建后根据角色设置
 		user.SetSetting(defaultSetting)
 	}
