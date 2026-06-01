fdffa742e8fcf6195cc1d07702df3aa4c6ef0c23
wuyongxin <wuyx@tydic.com>
2026-06-01 19:29:30 +0800

    固定背景色

--- web/src/hooks/dashboard/useDashboardCharts.jsx
+++ web/src/hooks/dashboard/useDashboardCharts.jsx
@@ -50,6 +50,7 @@ export const useDashboardCharts = (
   // ========== 图表规格状态 ==========
   const [spec_pie, setSpecPie] = useState({
     type: 'pie',
+    background: '#ffffff',
     data: [
       {
         id: 'id0',
@@ -107,6 +108,7 @@ export const useDashboardCharts = (
 
   const [spec_line, setSpecLine] = useState({
     type: 'bar',
+    background: '#ffffff',
     data: [
       {
         id: 'barData',
@@ -172,7 +174,7 @@ export const useDashboardCharts = (
           });
           return array;
         },
-      },
+      },ldpg
     },
     color: {
       specified: modelColorMap,
@@ -182,6 +184,7 @@ export const useDashboardCharts = (
   // 模型消耗趋势折线图
   const [spec_model_line, setSpecModelLine] = useState({
     type: 'line',
+    background: '#ffffff',
     data: [
       {
         id: 'lineData',
@@ -218,6 +221,7 @@ export const useDashboardCharts = (
   // 模型调用次数排行柱状图
   const [spec_rank_bar, setSpecRankBar] = useState({
     type: 'bar',
+    background: '#ffffff',
     data: [
       {
         id: 'rankData',
